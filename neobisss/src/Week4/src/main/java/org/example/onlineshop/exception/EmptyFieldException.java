f602d7a439cb05d87f774b429c083b45a2a1555e
Asadbek Beganov <134205232+asadaravani@users.noreply.github.com>
2024-03-24 17:58:31 +0600

    Delete neobisss/src/Week4 directory
    replacing with new Week4

--- neobisss/src/Week4/src/main/java/org/example/onlineshop/exception/EmptyFieldException.java
+++
@@ -1,7 +0,0 @@
-package org.example.onlineshop.exception;
-
-public class EmptyFieldException extends RuntimeException{
-    public EmptyFieldException(String message){
-        super(message);
-    }
-}
--- neobisss/src/Week4/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-
-insert into Product(product_name, price) values ( 'Asad', 149.99);
-insert into Product(product_name, price) values ( 'Ahmed', 155.05 );
-insert into Product(product_name, price) values ( 'Mehmet', 152.94 );
-insert into Product(product_name, price) values ( 'Janibek', 100.01 );
